a2c4c30b3648e1811314d188bd2dfff7cb577b8f
geeteshpillarplus <geetesh@pillarplus.co>
2022-05-09 15:24:05 +0530

    prisma version updated

--- apps/server/src/prisma/generated/type-graphql/helpers.ts
+++ apps/server/src/prisma/generated/type-graphql/helpers.ts
@@ -15,10 +15,10 @@ export function transformFields(
 }
 
 export function getPrismaFromContext(context: any) {
-  const prismaClient = context.prisma;
+  const prismaClient = context["prisma"];
   if (!prismaClient) {
     throw new Error(
-      "Unable to find Prisma Client in GraphQL context. Please provide it under the `context.prisma` key.",
+      'Unable to find Prisma Client in GraphQL context. Please provide it under the `context["prisma"]` key.',
     );
   }
   return prismaClient;
